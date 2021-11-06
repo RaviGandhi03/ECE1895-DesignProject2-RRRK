ef9c48536bacd32a0c385b028f3d02d6ca623244
Kamdens11 <48361198+Kamdens11@users.noreply.github.com>
2021-11-06 16:46:24 -0400

    Add files via upload
    this has more stuff done, still need to figure out SD card reading but I have a good start to a lot of the tasks I think

--- Bop-it_pseudo.ino
+++ Bop-it_pseudo.ino
@@ -1,21 +1,72 @@
 //********DIGITAL PIN INPUTS FOR BUTTTONS ARE PULLED HIGH SO PRESSING BUTTON MAKES INPUT GO LOW
+//include files for LCD display
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+
+
+//TASK 1= BUTTON
+//TASK 2=JOYSTICK
+//TASK 3=ULTRASONIC SENSOR
+
+//declare variable for LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+//declare variables for buttons
+int buttonTask=4;
+int startPin=0;
+//joystick
+int joyX=A0;
+int joyY=A1;
+int sw=1;
+//sensor
+int trig=3;
+int echo=2;
+//declare variable for ultrasonic sensor threshold
+int threshold=20;
+//other variables for sensor
+long duration;
+int distance;
+
+//LED pins
+int greenLED=6;
+int redLED=7;
+
 void setup() {
   // put your setup code here, to run once:
+  //initilize lcd display
+  lcd.init();
+  lcd.backlight();
   //create pin input and outputs
+  pinMode(joyX, OUTPUT);
+  pinMode(joyY, OUTPUT);
+  pinMode(sw, INPUT_PULLUP);
+  pinMode(buttonTask, INPUT_PULLUP);
+  pinMode(startPin, INPUT_PULLUP);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //creat variables to keep track of score;
+  //creat variables to keep track of score and set equal to 0 before start of each game;
   int score=0;
+  //make sure all outputs are low to start
+  digitalWrite(greenLED, LOW);
+  digitalWrite(redLED, LOW);
   //wait to see if start button has been pressed
-  while(digitalRead(StartPin)){
+  while(digitalRead(startPin)){
     //stay here until start button is pressed
   }
+  //immediately initialize score to 0 on lcd display
+  displayScore(score);
   //once button is pressed play game until score reaches 100 or incorrect response
   while(score<=99){
   //set digital outputs all low
+  digitalWrite(greenLED, LOW);
+  digitalWrite(redLED, LOW);
   //generate a task to generate correct answer, task to be performed
   int correctTask= getRandomTask();
   //figure out how much time user will have to make correct response
@@ -31,15 +82,17 @@ void loop() {
   //wait for response or answer
   while((millis()-previousTime)<=decisionTime+10){
     //check to see what buttun was pressed
-    if(!digitalRead(taskOneInput)){
+    if(!digitalRead(buttonTask)){
       userResponse=1;
       break;
     }
-    else if(!digitalRead(taskTwoInput)){
+    //joystick movement to see if joystick was moved
+    else if(joystickMovement()){
       userResponse=2;
       break;
     }
-    else if(!digitalRead(taskThreeInput)){
+    //sensor check to see how close to ultrasonic sensor
+    else if(sensorCheck()){
       userResponse=3;
       break;
     }
@@ -52,8 +105,9 @@ void loop() {
   //check to see if user picked correct response
   if(userResponse==correctTask){
     //notify user correct score was made
+    
     //increment score and display score
-    score++
+    score++;
     displayScore(score);
   }
   else{
@@ -94,5 +148,43 @@ void congratulations(){
 
 //function to display score
 void displayScore(int score){
-  
+ //clear previous selections from lcd display
+ lcd.clear();
+ lcd.setCursor(5, 0);
+ lcd.print("Score: ");
+ lcd.setCursor(7,1);
+ lcd.print(score);
+}
+
+//joystick movement function
+bool joystickMovement(){
+  //write code to see if joystick was moved
+  if(analogRead(joyX)>10 || analogRead(joyY)>10 ||!digitalRead(1)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+//sensor check funtion
+bool sensorCheck(){
+  //clear trig
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  //set trig high for 10 us
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  //set duration
+  duration=pulseIn(echo, HIGH);
+
+  //calculate disatnce in cm
+  distance= duration*(0.034/2);
+  if(distance<=threshold){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
